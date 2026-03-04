4e76b18c6ecea194a4e155eef4f0073c96fd2258
Guru <gpraokp23@gmail.com>
2026-03-04 03:43:15 +0000

    publishing new change

--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # Retrieve environment variable or use default
-    message = os.environ.get("APP_MESSAGE", "***  Hello from GKE Demo, NTT Niveus! Welcome***")
+    message = os.environ.get("APP_MESSAGE", "***  Hello from GKE!***")
     return f"<p>{message}</p>"
 
 if __name__ == "__main__":
